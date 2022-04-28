3af23ea6d1ababacddc0a62acedd9057fc97da64
JakesProjects1 <jacob@kagon.net>
2022-04-28 15:31:22 -0400

    unhide cloudinary files

---
+++ config/initializers/cloudinary.rb
@@ -0,0 +1,9 @@
+require 'cloudinary'
+
+Cloudinary.config do |config|    
+    config.cloud_name = "dcqo0ndth"   
+    config.api_key = "264484237654179"   
+    config.api_secret = "v78YgnCnDycRp-y8FMytAdYtoMo"   
+    config.secure = true    
+    config.cdn_subdomain = true  
+ end
