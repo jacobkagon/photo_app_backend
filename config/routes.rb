f2f854b5e2375f638cef3f7902312f4e2279fc70
JakesProjects1 <jacob@kagon.net>
2022-05-02 18:07:39 -0400

    fix routes

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
       resources :photos, only: [:index, :create]
       resources :categories, only: [:index, :create]
 
-      get ':id/photos', to: 'categories#photos'
+      get 'categories/:id', to: 'categories#photos'
     end
   end
 end
